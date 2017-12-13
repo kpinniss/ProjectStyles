9c0758cf0cbd0bba6003e2fbb8dbfe4d543c978c
kris <kpinniss@gmail.com>
2017-12-12 23:19:00 -0700

    more updates

--- app.js
+++ app.js
@@ -16,22 +16,117 @@ var items =[
     {name:"large Green Charm", price:4.99, category:"charms", size:2},
 ];
 
+var cart = {
+    items:[],
+    locket:{name:"", price:0},
+    chain:{name:"", price:0},
+    total:function(){
+        var result = 0;
+        for(var i = 0; i < this.items.length; i++){
+            result += this.items[i].price
+        }
+        var formatter = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+            minimumFractionDigits: 2,
+            // the default value for minimumFractionDigits depends on the currency
+            // and is usually already 2
+          });
+          result += (this.locket.price + this.chain.price);
+          result = formatter.format(result);
+            return result;
+    }
+};
+
+var vLocket = {
+    charms:[],
+    locket: {},
+    chain:{},
+    actual: function(){
+        var actualSize = 0;
+        for(var i =0; i < this.charms.length; i++){
+            actualSize += this.charms[i].size;
+        }
+        return actualSize;
+    }, 
+    max_capacity:8, //make actual check itself and return its own vals 
+    checkCapacity: function(){
+        if(this.actual() > this.max_capacity){
+            alert("Max capacity met please remove some charms from your locket.");
+        }
+    }
+
+};
+
 
 var app = angular.module("Locket-Builder", []);
 app.controller("Main", function ($scope, $http) {
     $scope.categories = categories;
-    
-    //get chains
-    $scope.getItems = function(category){
+    $scope.chains =  getItems("chains");
+    $scope.charms = getItems("charms");
+    $scope.lockets = getItems("lockets");
+    $scope.showTable = false;
+    $scope.showCategory = "chains";
+    $scope.cart = cart;
+    $scope.vLocket = vLocket;
+
+    function getItems(category){
         var itemsToReturn = [];
         for(var i =0; i < items.length; i++){
             if(items[i].category == category){
                 itemsToReturn.push(items[i]);
             }
         }
-         createTable(itemsToReturn, category);
+         return itemsToReturn;
     }
     
+    $scope.show = function(show,category){
+        if(show == true){
+            $scope.showTable = false;
+            $scope.showCategory = "";
+        }
+        else{
+            $scope.showTable = true;
+            $scope.showCategory = category
+        }
+    }
+
+    function remove(array, element) {
+        const index = array.indexOf(element);
+        array.splice(index, 1);
+    }
+
+    $scope.addToCart = function(item){
+        var addItem = {
+            name: item.name,
+            price: item.price,
+            category:item.category
+        };
+        var locketItem = {
+            size:item.size
+        };
+        var isLower = $scope.cart.items.length >= 1;
+      
+           
+        if(addItem.category =="chains"){
+            $scope.cart.chain.price = addItem.price;
+            $scope.cart.chain.name = addItem.name;
+        }
+        if(addItem.category =="lockets"){
+            $scope.cart.locket.price = addItem.price;
+            $scope.cart.locket.name = addItem.name;
+        }
+        else{
+            $scope.vLocket.charms.push(locketItem);
+            $scope.cart.items.push(addItem);
+            $scope.vLocket.actual();
+            $scope.vLocket.checkCapacity();
+        }
+        
+        /////
+        
+    }
+
     function createTable(items, category){
         if($('#'+category+' tr').length > 0){
             $('#'+category).empty();
